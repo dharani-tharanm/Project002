faa7ac6c10c62eab1d15c76d965958f8249f5491
dharani-tharanm <dharanitharan2019@gmail.com>
2024-08-16 15:38:52 +0530

    Changes happen in Index.cshtml

--- TodoApp/obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
+++ TodoApp/obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+570e4597d55f75315766119ba436e5c8433cd84f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
